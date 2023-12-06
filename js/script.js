f02299879b5914ee5ae0570af97ad06db48d807a
xJustFox <dodydaversa@gmail.com>
2023-12-06 16:54:09 +0100

    [Fix] Added comments

--- js/script.js
+++ js/script.js
@@ -25,9 +25,12 @@ let bikeArray = [
         weight: 12
     },
 ]
-
+// Scrollo gli oggetti contenuti nell'array
 bikeArray.forEach((elem) => {
+    // destructuring
     let {name, weight} = elem;
+
+    // se il peso è minore di 25 verra printato in console
     if (weight <= 25) {
         console.log(elem);
     }
@@ -69,17 +72,49 @@ let footballTeams = [
 ]
 console.log(footballTeams);
 
+// Creo un array vuoto
 let foulsTeams = []
+
+// Scrollo gli oggetti contenuti nell'array
 footballTeams.map((elem) => {
+    // destructuring
     let {name, goal, fouls} = elem;
 
-    if (goal == 0) {
+    // Utilizzo un if per filtrare i valori che voglio cambiare
+    if (goal == 0 && fouls == 0) {
+        // genero randomicamente i valori e gli aggiungo alle proprietà che devono essere cambiate
         elem.goal = Math.floor(Math.random()*10 + 0);
         elem.fouls = Math.floor(Math.random()*10 + 0);
         console.log(elem);
+
+        // elimino la proprietà goal
         delete elem.goal;
+
+        // inserisco i nuovi oggetti nell'array vuoto
         foulsTeams.push(elem)
     }
 })
-delete foulsTeams['goal'];
 console.log(foulsTeams);
+
+
+// SNACK 3
+let articles = [
+    {
+        name: 'Poppy',
+        type: 'tshirt',
+        color: 'red'
+    },
+    {
+        name: 'Jumping',
+        type: 'occhiali',
+        color: 'blue'
+    },{
+        name: 'CrissCross',
+        type: 'scarpe',
+        color: 'black'
+    },{
+        name: 'Jenny',
+        type: 'borsa',
+        color: 'pink'
+    }
+]
